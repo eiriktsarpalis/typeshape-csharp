91f6f2aa1853383ea2bee0c12dd8927b6dcca18a
Eirik George Tsarpalis <eirik.tsarpalis@gmail.com>
2023-11-01 17:47:25 +0000

    Use quotes in diagnostic messages.

--- src/TypeShape.SourceGenerator/ModelGenerator/ModelGenerator.Diagnostics.cs
+++ src/TypeShape.SourceGenerator/ModelGenerator/ModelGenerator.Diagnostics.cs
@@ -18,7 +18,7 @@ public sealed partial class ModelGenerator
     private static DiagnosticDescriptor TypeNotSupported { get; } = new DiagnosticDescriptor(
         id: "TS0001",
         title: "Type shape generation not supported for type.",
-        messageFormat: "Type shape generation not supported for type {0}.",
+        messageFormat: "Type shape generation not supported for type '{0}'.",
         category: "TypeShape.SourceGenerator",
         defaultSeverity: DiagnosticSeverity.Warning,
         isEnabledByDefault: true);
@@ -26,7 +26,7 @@ public sealed partial class ModelGenerator
     private static DiagnosticDescriptor ProviderTypeNotPartial { get; } = new DiagnosticDescriptor(
         id: "TS0002",
         title: "Type annotated with GenerateShapeAttribute is not partial.",
-        messageFormat: "The type {0} has been annotated with GenerateShapeAttribute but it or one of its parent types are not marked partial.",
+        messageFormat: "The type '{0}' has been annotated with GenerateShapeAttribute but it or one of its parent types are not marked partial.",
         category: "TypeShape.SourceGenerator",
         defaultSeverity: DiagnosticSeverity.Error,
         isEnabledByDefault: true);
