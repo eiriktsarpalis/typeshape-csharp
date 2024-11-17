80415f4d7a49e2383875f470d4e8a59b3d3baa82
Eirik Tsarpalis <eirik.tsarpalis@gmail.com>
2024-11-17 18:03:32 +0000

    Improve API documentation consistency.

--- src/PolyType/GenerateShapeAttribute.cs
+++ src/PolyType/GenerateShapeAttribute.cs
@@ -3,15 +3,15 @@
 /// <summary>
 /// When applied on a partial type, this attribute instructs the
 /// PolyType source generator to emit an <see cref="IShapeable{T}"/>
-/// implementation for the annotated type.
+/// implementation that includes the annotated type.
 /// </summary>
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct | AttributeTargets.Interface, AllowMultiple = false, Inherited = false)]
 public sealed class GenerateShapeAttribute : Attribute;
 
 /// <summary>
-/// When applied on a partial class, instructs the PolyType source generator
-/// to emit an <see cref="ITypeShapeProvider"/> implementation that
-/// includes a shape definition for <typeparamref name="T"/>.
+/// When applied on a partial class, this attribute instructs the
+/// PolyType source generator to emit an <see cref="ITypeShapeProvider"/>
+/// implementation that includes <typeparamref name="T"/>.
 /// </summary>
 /// <typeparam name="T">The type for which shape metadata will be generated.</typeparam>
 [AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = false)]
